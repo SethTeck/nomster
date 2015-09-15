9860c223d667f84582b69a413240cb541769f619
Seth Allen <seth.a@me.com>
2015-09-15 18:53:20 +0000

    Generate new Mailer for comment norification

---
+++ app/mailers/notification_mailer.rb
@@ -0,0 +1,9 @@
+class NotificationMailer < ActionMailer::Base
+  default from: "no-reply@nomsterapp.com"
+
+  def comment_added
+  	mail(to: "seth@sethteck.com",
+  		subject: "A comment has been added to one of your places")
+  end
+
+end
