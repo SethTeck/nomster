4206a8ced7e9778665fb8194f70e666ac9750fe1
Seth Allen <seth.a@me.com>
2015-09-15 19:13:27 +0000

    Attempting to send test email to my gmail address

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ActionMailer::Base
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-  	mail(to: "seth@sethteck.com",
+  	mail(to: "goletastudio@gmail.com",
   		subject: "A comment has been added to one of your places")
   end
 
