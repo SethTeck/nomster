84ffa2717cf928532e2a8ebcb53449fa96be2e47
Seth Allen <seth.a@me.com>
2015-09-27 00:50:02 +0000

    Built tests for comment rating functionality, User Dashboard, and Comment creation functionality

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
   test "comment rating functionality" do
-  	rating = FactoryGirl.create(:comment, :rating => '5_stars')
+  	comment = FactoryGirl.create(:comment, :rating => '5_stars')
   	expected = 'five stars'
   	actual = comment.humanized_rating
   	assert_equal expected, actual
